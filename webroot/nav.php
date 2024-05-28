560d7727bf74251a221891e238c8c64881a3489d
suttha jongjaroen <6410615121@cn330>
2024-05-28 14:18:49 +0700

    add summary on nav

--- webroot/nav.php
+++ webroot/nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-dark bg-black p-3">
         <div class="ms-3">
             <a href="http://127.0.0.1:8001/index.php" class="navbar-brand">Home</a>
-            <a href="http://127.0.0.1:8001/task_form.php" class="navbar-brand">Summary</a>
+            <a href="http://127.0.0.1:8001/summary.php" class="navbar-brand">Summary</a>
         </div>
         <div class="me-3">
             <a href="http://127.0.0.1:8001/task_form.php" class="navbar-brand">Task Add</a>
