fefdc85851fab0a7754f0424a0ef9ccc18a2c729
suttha jongjaroen <6410615121@cn330>
2024-05-28 14:18:41 +0700

    update index_view

--- webroot/index_view.php
+++ webroot/index_view.php
@@ -26,52 +26,52 @@
 
 
         <main>
-            <div class="p-5">
-                <div class="d-flex flex-row justify-content-end">
-                    <form action="" class="form" method="post">
-                        <div class="d-flex flex-row me-3">
-                            <input type="date" class="form-control me-2" name="date">
-                            <input type="hidden" name="action_date_search" value="1">
-                            <input type="submit" class="btn btn-primary" value="Date Search">
-                        </div>
-                    </form>
+            <div class="p-3">
+                <div style="padding: 0 5em;">
+                    <div class="d-flex flex-row justify-content-end">
+                        <form action="" class="form" method="post">
+                            <div class="d-flex flex-row me-3">
+                                <input type="date" class="form-control me-2" name="date">
+                                <input type="hidden" name="action_date_search" value="1">
+                                <input type="submit" class="btn btn-primary" value="Date Search">
+                            </div>
+                        </form>
 
-                    <form action="" class="form" method="post">
-                        <div class="d-flex flex-row mb-3">
-                            <input type="month" class="form-control me-2" name="month">
-                            <input type="hidden" name="action_month_search" value="1">
-                            <input type="submit" class="btn btn-primary" value="Month Search">
-                        </div>
-                    </form>
+                        <form action="" class="form" method="post">
+                            <div class="d-flex flex-row mb-3">
+                                <input type="month" class="form-control me-2" name="month">
+                                <input type="hidden" name="action_month_search" value="1">
+                                <input type="submit" class="btn btn-primary" value="Month Search">
+                            </div>
+                        </form>
+                    </div>
+                    <table class="table table-bordered table-striped align-middle text-center">
+                        <th>Task Type</th>
+                        <th>Task title</th>
+                        <th>Start time</th>
+                        <th>End time</th>
+                        <th>Status</th>
+                        <th>Added Time</th>
+                        <th>Modified Time</th>
+                        <th>Update</th>
+                        <th>Delete</th>
+                    
+                        <?php foreach($tasks as $task): ?>
+                            <tr class="">
+                                <td><?= $task['task_type'] ?></td>
+                                <td><?= $task['task_name'] ?></td>
+                                <td><?= $task['task_period_start'] ?></td>
+                                <td><?= $task['task_period_end'] ?></td>
+                                <td><?= $task['task_status'] ?></td>
+                                <td><?= $task['record_timestamp'] ?></td>
+                                <td><?= $task['last_update_timestamp'] ?></td>
+                                <td><a href="./task_update.php/?id=<?= $task['id'] ?>" class="btn btn-primary">Update</a></td>
+                                <td><a href="./index.php?delete_task_id=<?= $task['id'] ?>" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this task?')">Delete</a></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </table>
                 </div>
 
-
-
-                <table class="table table-bordered table-striped align-middle text-center">
-                    <th>Task Type</th>
-                    <th>Task title</th>
-                    <th>Start time</th>
-                    <th>End time</th>
-                    <th>Status</th>
-                    <th>Added Time</th>
-                    <th>Modified Time</th>
-                    <th>Update</th>
-                    <th>Delete</th>
-                
-                    <?php foreach($tasks as $task): ?>
-                        <tr class="">
-                            <td><?= $task['task_type'] ?></td>
-                            <td><?= $task['task_name'] ?></td>
-                            <td><?= $task['task_period_start'] ?></td>
-                            <td><?= $task['task_period_end'] ?></td>
-                            <td><?= $task['task_status'] ?></td>
-                            <td><?= $task['record_timestamp'] ?></td>
-                            <td><?= $task['last_update_timestamp'] ?></td>
-                            <td><a href="./task_update.php/?id=<?= $task['id'] ?>" class="btn btn-primary">Update</a></td>
-                            <td><a href="./index.php?delete_task_id=<?= $task['id'] ?>" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this task?')">Delete</a></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </table>
             </div>
         </main>
 
