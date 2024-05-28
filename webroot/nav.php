ee005719cb9012897d1644fff3781bdaac1a2b0e
suttha jongjaroen <6410615121@cn330>
2024-05-28 13:20:16 +0700

    update nav.php

--- webroot/nav.php
+++ webroot/nav.php
@@ -1,6 +1,11 @@
-<nav class="navbar navbar-dark bg-black p-2">
-    <div class="container">
-         <a href="http://127.0.0.1:8001/index.php" class="navbar-brand">Home</a>
-         <a href="http://127.0.0.1:8001/task_form.php" class="navbar-brand">Task Add</a>
+<nav class="navbar navbar-dark bg-black p-3">
+        <div class="ms-3">
+            <a href="http://127.0.0.1:8001/index.php" class="navbar-brand">Home</a>
+            <a href="http://127.0.0.1:8001/task_form.php" class="navbar-brand">Summary</a>
+        </div>
+        <div class="me-3">
+            <a href="http://127.0.0.1:8001/task_form.php" class="navbar-brand">Task Add</a>
+        </div>
+
     </div>
 </nav>
